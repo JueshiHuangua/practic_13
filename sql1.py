751e191f10956ef709f18c0cd0b6a725bec9f9d5
JueshiHuangua <pirojok_5@mail.ru>
2026-03-04 14:31:18 +0500

    4А. ORDER BY

--- sql1.py
+++ sql1.py
@@ -1,19 +1,13 @@
 import sqlite3
 
+
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age')
+cursor.execute('SELECT username, age FROM Users ORDER BY age DESC')
 results = cursor.fetchall()
 
 for row in results:
     print(row)
 
-cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age HAVING AVG(age) > ?', (30,))
-filtered_results = cursor.fetchall()
-
-for row in filtered_results:
-    print(row)
-
 connection.commit()
-connection.close()
