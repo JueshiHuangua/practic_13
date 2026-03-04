e480b1945e72ac59880ea5deddce03952c1a41e4
JueshiHuangua <pirojok_5@mail.ru>
2026-03-04 14:27:26 +0500

    4А. GROUP BY и HAVING

--- sql1.py
+++ sql1.py
@@ -1,13 +1,18 @@
 import sqlite3
 
-
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-cursor.execute('SELECT username, age FROM Users WHERE age > ?', (25,))
+cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age')
 results = cursor.fetchall()
 
 for row in results:
+    print(row)
+
+cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age HAVING AVG(age) > ?', (30,))
+filtered_results = cursor.fetchall()
+
+for row in filtered_results:
     print(row)
 
 connection.commit()
