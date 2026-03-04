6da75ab08e58e8bd4be76f31074601149f927d2e
JueshiHuangua <pirojok_5@mail.ru>
2026-03-04 15:21:26 +0500

    5A

--- sql1.py
+++ sql1.py
@@ -4,10 +4,16 @@ connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
 cursor.execute('SELECT * FROM Users')
-users = cursor.fetchall()
+first_user = cursor.fetchone()
+print(first_user)
 
-for user in users:
-    print(user)
+cursor.execute('SELECT * FROM Users')
+first_five_users = cursor.fetchmany(5)
+print(first_five_users)
 
+cursor.execute('SELECT * FROM Users')
+all_users = cursor.fetchall()
+print(all_users)
+    
 connection.commit()
 connection.close()
