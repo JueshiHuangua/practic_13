2efbdff96cab533cdc8c3fdb478518cf913e4c20
JueshiHuangua <pirojok_5@mail.ru>
2026-03-04 12:53:57 +0500

    Три. Добавление юзера

--- sql1.py
+++ sql1.py
@@ -4,9 +4,8 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-cursor.execute('''
-    CREATE INDEX idx_email ON Users (email)
-''')
+cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', 
+               ('newuser', 'newuser@example.com', 28))
 
 connection.commit()
 connection.close()
