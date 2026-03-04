2ed1f614c8fd409c3210e5d3ec9aad79892c741f
JueshiHuangua <pirojok_5@mail.ru>
2026-03-04 12:50:49 +0500

    Вторая A практическая

--- sql1.py
+++ sql1.py
@@ -5,12 +5,7 @@ connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Users (
-        id INTEGER PRIMARY KEY,
-        username TEXT NOT NULL,
-        email TEXT NOT NULL,
-        age INTEGER
-    )
+    CREATE INDEX idx_email ON Users (email)
 ''')
 
 connection.commit()
