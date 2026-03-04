866f99d430b541d2318996f9b3a5def87f17cc0c
JueshiHuangua <pirojok_5@mail.ru>
2026-03-04 13:21:11 +0500

    Четыре. Запрос все юзеры

--- sql1.py
+++ sql1.py
@@ -1,9 +1,20 @@
 import sqlite3
 
+
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-cursor.execute('DELETE FROM Users WHERE username = ?', ('newuser',))
+cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', 
+               ('newuser', 'newuser@example.com', 28))
+cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', 
+               ('user1', 'newuser1@example.com', 25))
 
 connection.commit()
+
+cursor.execute('SELECT * FROM Users')
+users = cursor.fetchall()
+
+for user in users:
+    print(user)
+    
 connection.close()
