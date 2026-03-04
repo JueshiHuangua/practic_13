17f54ab06c44aa50a8b3d1e05bfb9434e32a28f2
JueshiHuangua <pirojok_5@mail.ru>
2026-03-04 14:46:40 +0500

    4B. Комбинирование операторов

--- sql1.py
+++ sql1.py
@@ -1,13 +1,19 @@
 import sqlite3
 
-
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-cursor.execute('SELECT username, age FROM Users ORDER BY age DESC')
+cursor.execute('''
+    SELECT username, age, AVG(age) 
+    FROM Users 
+    GROUP BY age 
+    HAVING AVG(age) > ? 
+    ORDER BY age DESC
+''', (30,))
 results = cursor.fetchall()
 
 for row in results:
     print(row)
 
 connection.commit()
+connection.close()
