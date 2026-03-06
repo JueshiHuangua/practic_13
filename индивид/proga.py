fe16364d90c2f108cc3fade3307f58e1ec9792bb
JueshiHuangua <pirojok_5@mail.ru>
2026-03-06 23:15:09 +0500

    Вывод данных

--- индивид/proga.py
+++ индивид/proga.py
@@ -13,10 +13,26 @@ class main_window(QWidget):
 
         self.read_furniture()
 
-    def read_partners(self):
+    def read_furniture(self):
         self.ui.tableWidget.setRowCount(0)
-        cursor.execute('SELECT * FROM Partners')
-        self.partners_data = cursor.fetchall()
+        cursor.execute('SELECT * FROM мебель')
+        self.data = cursor.fetchall()
+        print(self.data)
+                
+        self.ui.tableWidget.setRowCount(len(self.data))
+        self.ui.tableWidget.setColumnCount(1)
+        for row in range(len(self.data)):
+            text = ('\n' + str(self.data[row][1]) + ' | ' + 
+                str(self.data[row][7]) + '\nКоличество предметов: ' + 
+                str(self.data[row][3]) + '\nМатериал: ' + 
+                str(self.data[row][4]) + '\nЦвет: ' + 
+                str(self.data[row][5]) + '\nЦена: ' + 
+                str(self.data[row][6]) + ' руб.\nИзготовитель: ' + 
+                str(self.data[row][2]) + '\n')
+            item = QTableWidgetItem()
+            item.setText(text)
+            self.ui.tableWidget.setItem(row, 0, item)
+        self.ui.tableWidget.resizeRowsToContents()
 
 
 app = QApplication(sys.argv)
